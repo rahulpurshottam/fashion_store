ba309b3d158fc6d5c52a02d53c101e7ded58267c
rahulpurshottam <r77933079@gmail.com>
2025-06-04 14:51:41 +0530

    Update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -11,7 +11,7 @@ const LatestCollection = () => {
         setLatestProducts(products.slice(0,10));
     },[products])
   return (
-    <div className='my-10'>
+ <div id="latestcollection" className='my-10'>
         <div className="text-center py-8 text-3xl">
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
